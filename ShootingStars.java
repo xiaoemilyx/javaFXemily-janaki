709b86f15796217b064b7557e328b96738e7814f
emily <563723@my.cuhsd.org>
2026-05-20 10:58:34 -0700

    Add files via upload

---
+++ ShootingStars.java
@@ -0,0 +1,15 @@
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ShootingStars{
+    
+}
